e08ec58c74ff933ed7169c32b8bbadaa7b57e1c9
janghe11 <jang0913@gmail.com>
2015-12-17 02:50:47 +0900

    20151217_implemented wget data

--- getData.sh
+++ getData.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 #카테고리에 있는 view?no 목록 받아오기
-for ((i=4; i<10; i++)); do
-	getCate = wget http://hmscience.co.kr/goods/catalog?code=000$i
-	getView =
 
-	eval 'echo | sed ""' 
-	
+getCate=/home/jang/Projects/hmscience/catalog_code
+getView=/home/jang/Projects/hmscience/getView.log
+
+#카테고리 code를 얻기 위한 변수 codeNum 생성
+codeNum=4
+
+while [ "$codeNum" -lt 10 ]
+do
+	wget -P $getCate "http://hmscience.co.kr/goods/catalog?code=000$codeNum"
+	sed -n '/.view?no/p' $getCate/catalog?code=000$codeNum >> $getView
+	codeNum=$((codeNum+1))
 done
 
-#view?no 지정
-for ((i=0; i < 9999; i(++)); do
-	wget http://hmscience.co.kr/goods/view?no=$i
-	
-done
+wget -P $getCate "http://hmscience.co.kr/goods/catalog?page=2&code=0005"
+sed -n '/.view?no/p' "$getCate/catalog?page=2&code=0005" >> $getView
 
-#view?no 전체를 받는 사이트 변수 지정
-getPage
+#view?no의 숫자를 얻어오기
+viewNumPrepend=\/view\?no"="
+viewNum=/home/jang/Projects/hmscience/viewNum.log
+
+echo "$getView" | sed -e 's/[v][i][e][w][?][n][o][=][0-9][0-9]/ &/'
+
+#echo'expr $getView
+
+#"[v][i][e][w][?][n][o][=][0-9][0-9]"
+#"[v][i][e][w][?][n][o][=][0-9][0-9][0-9]"
+#"[v][i][e][w][?][n][o][=][0-9][0-9][0-9][0-9]"
